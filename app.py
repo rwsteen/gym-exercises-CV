1cec745580396dc2fe4492e60b95ecf98650e289
rvantienen <rvantienen@tudelft.nl>
2026-03-26 11:32:10 +0100

    Add stable old version snapshot

---
+++ app.py
@@ -0,0 +1,240 @@
+# run app with "streamlit run app.py"
+
+import streamlit as st
+import cv2
+import mediapipe as mp
+import numpy as np
+import torch
+import csv
+from model.model import STGCN
+from form import form_analysis
+
+# load model
+NUM_CLASSES = 2
+NUM_JOINTS = 13
+model = STGCN(num_class=NUM_CLASSES, num_point=NUM_JOINTS)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+model.load_state_dict(torch.load("stgcn_model4.pth", map_location=device))
+model.eval()
+
+# Penn Action dataset labels
+exercise_labels = [
+    "squat",
+    "pushup"
+]
+
+# MediaPipe pose landmarks to Penn Action joints mapping
+PENN_JOINTS = [
+    0,   # head
+    11,  # left_shoulder
+    12,  # right_shoulder
+    13,  # left_elbow
+    14,  # right_elbow
+    15,  # left_wrist
+    16,  # right_wrist
+    23,  # left_hip
+    24,  # right_hip
+    25,  # left_knee
+    26,  # right_knee
+    27,  # left_ankle
+    28   # right_ankle
+]
+
+def extract_penn_joints(results, V=13, C=2):
+    joints = np.zeros((V, C)) # (V, C)
+
+    if results.pose_landmarks:
+        landmarks = results.pose_landmarks.landmark
+
+        for i, mp_idx in enumerate(PENN_JOINTS):
+            joints[i] = [landmarks[mp_idx].x, landmarks[mp_idx].y]
+    
+    return joints
+    
+
+# Initialize MediaPipe Pose and Drawing utilities
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+
+st.title("Exercise Detection App")
+
+video_source = st.selectbox(
+    "Select Input Source",
+    ("Webcam", "Video File")
+)
+
+video_file = None
+
+if video_source == "Video File":
+    video_file = st.file_uploader("Upload a video", type=["mp4", "mov"])
+
+start = st.button("Start")
+
+frame_placeholder = st.empty()
+
+
+
+if start:
+
+    # Set up video capture based on the selected source
+    if video_source == "Webcam":
+        cap = cv2.VideoCapture(0)
+    else:
+        tfile = open("temp.mp4", "wb")
+        tfile.write(video_file.read())
+        cap = cv2.VideoCapture("temp.mp4")
+
+    frame_count = 0
+
+    # Process video frames and extract pose landmarks with MediaPipe
+    with mp_pose.Pose(
+        model_complexity=2,
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    ) as pose:
+
+        T = 16 # Number of frames to process for each sample (for model input)
+        V = 13 # Number of joints (Penn Action dataset)
+        C = 2 # only x and y coordinates as channels
+
+        joint_buffer = [] # buffer to hold joint data for T frames
+        pred_action = "N/A"
+        pred_count = 0
+        pred_shallow_rep = 0
+        deepnes = 0
+        prev_phase = "N/A"
+        frame_skip = 4 # skip frames to reduce computation time
+        back_hollow = 0
+        rounded_back = 0
+        perfect_form = 0
+        forward_bend2 = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            if frame_count % frame_skip != 0:
+                frame_count += 1
+                continue
+
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = cv2.resize(image, (320, 240))
+            results = pose.process(image)
+
+            if results.pose_landmarks:
+                joints = []
+                row = [frame_count]
+
+                for lm in results.pose_landmarks.landmark:
+                    row += [lm.x, lm.y, lm.z, lm.visibility]
+                
+                joints = extract_penn_joints(results, V, C) # (13, 2)
+
+                # Add joints to buffer
+                joints = np.array(joints) # (V, 2)
+                joint_buffer.append(joints)
+
+                if len(joint_buffer) > T:
+                    joint_buffer.pop(0) # keep only the last T frames
+
+                if len(joint_buffer) == T:
+                    x = torch.from_numpy(np.array(joint_buffer).transpose(2,0,1)[None,...,None]).float().to(device) # (1, C, T, V, 1)
+                    with torch.no_grad():
+                        action, phase = model(x)
+
+                    pred_action = exercise_labels[torch.argmax(action).item()]
+                    
+                    
+                    # get current phase from model
+                    phase_np = phase.squeeze().cpu().numpy()  # (T,)
+                    phase = phase_np[-1]
+                    print("phase", phase)
+                    
+                    #'''
+                    landmarks = results.pose_landmarks.landmark
+
+                    left_vis_pu = (
+                        landmarks[11].visibility +  # left shoulder
+                        landmarks[13].visibility    # left elbow
+                    )
+                    
+                    right_vis_pu = (
+                        landmarks[12].visibility +  # right shoulder
+                        landmarks[14].visibility    # right elbow
+                    )
+                    
+                    left_vis_s = (
+                        landmarks[23].visibility +  # left hip
+                        landmarks[25].visibility    # left knee
+                    )
+                    
+                    right_vis_s = (
+                        landmarks[24].visibility +  # right hip
+                        landmarks[26].visibility    # right knee
+                    )
+                    
+                    if pred_action == "pushup":
+                        # determine which side is more visible
+                        if left_vis_pu > right_vis_pu:
+                            visible_side = "left"
+                        else:
+                            visible_side = "right"
+                            
+                    if pred_action == "squat":
+                        # determine which side is more visible
+                        if left_vis_s > right_vis_s:
+                            visible_side = "left"
+                        else:
+                            visible_side = "right"
+                    
+                    if pred_action == "pushup":
+                        deep_enough, back_form = form_analysis(pred_action, prev_phase, x, visible_side)
+                        deepnes += deep_enough
+                        if back_form > 0:
+                            rounded_back += 1
+                        elif back_form < 0:
+                            back_hollow += 1
+                        else:
+                            perfect_form += 1
+                        print(deepnes)
+                    if pred_action == "squat":
+                        deep_enough, forward_bend = form_analysis(pred_action, prev_phase, x, visible_side)
+                        deepnes += deep_enough
+                        if forward_bend == 0:
+                            perfect_form += 1
+                        else:
+                            forward_bend2 += 1
+                    #'''
+
+                    if prev_phase == "N/A":
+                        prev_phase = "up" if phase > 0.5 else "down"
+                    elif prev_phase == "up" and phase < 0.4:
+                        prev_phase = "down"
+                    elif prev_phase == "down" and phase > 0.6:
+                        pred_count += 1
+                        prev_phase = "up"
+                        if deepnes == 0:
+                            pred_shallow_rep += 1
+                        deepnes = 0
+                
+                mp_drawing.draw_landmarks(
+                    frame,
+                    results.pose_landmarks,
+                    mp_pose.POSE_CONNECTIONS
+                )
+
+                cv2.putText(frame, f"Exercise: {pred_action}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                cv2.putText(frame, f"Count: {pred_count}", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                cv2.putText(frame, f"Shallow: {pred_shallow_rep}", (10, 110), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+                if pred_action == "pushup":
+                    cv2.putText(frame, f"Hollow back: {back_hollow}, Rounded back: {rounded_back}, Good form: {perfect_form}", (10, 450), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+                if pred_action == "squat":
+                    cv2.putText(frame, f"Forward bend: {forward_bend2}, Good form: {perfect_form}", (10, 800), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,0,255), 2)
+
+
+            frame_count += 1
+
+            frame_placeholder.image(frame, channels="BGR")
+
+    cap.release()
---
+++ train.py
@@ -0,0 +1,197 @@
+import os
+import torch
+import torch.nn as nn
+from preprocess import AugmentedPennActionDataset
+from model.model import STGCN
+from torch.utils.data import DataLoader, Subset
+from earlystopping import EarlyStopping
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from sklearn.model_selection import train_test_split
+
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA version (PyTorch):", torch.version.cuda)
+
+if torch.cuda.is_available():
+    print("GPU name:", torch.cuda.get_device_name(0))
+
+early_stopping = EarlyStopping(patience=15, delta=0.004)
+
+if __name__ == "__main__":
+    annotation_dir = "./augmentation/augmented_penn/labels"
+    
+    # load dataset
+    dataset = AugmentedPennActionDataset(annotation_dir, stride=1)
+    print(f"Total samples in dataset: {len(dataset)}")
+    print(f"dataset file labels distribution: {dataset.file_labels.count(0)} squats, {dataset.file_labels.count(1)} pushups")
+
+    # split dataset by file to avoid data leakage between train and test sets
+    train_file_idx, test_file_idx = train_test_split(
+        range(len(dataset.files)),
+        test_size=0.15,
+        stratify=dataset.file_labels,
+        random_state=42
+    )
+    train_val_labels = [dataset.file_labels[i] for i in train_file_idx]
+    train_file_idx, val_file_idx = train_test_split(
+        train_file_idx,
+        test_size=0.176,
+        stratify=train_val_labels,
+        random_state=42
+    )
+
+    train_files = [dataset.files[i] for i in train_file_idx]
+    test_files = [dataset.files[i] for i in test_file_idx]
+    val_files = [dataset.files[i] for i in val_file_idx]
+
+    train_indices = []
+    test_indices = []
+    val_indices = []
+    for i, (file_name, start) in enumerate(dataset.samples):
+        if file_name in train_files:
+            train_indices.append(i)
+        elif file_name in val_files:
+            val_indices.append(i)
+        elif file_name in test_files:
+            test_indices.append(i)
+
+    train_dataset = Subset(dataset, train_indices)
+    test_dataset = Subset(dataset, test_indices)
+    val_dataset = Subset(dataset, val_indices)
+
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
+
+    print(f"Dataset size: {len(dataset)}")
+
+    num_classes = len(dataset.allowed_actions)
+    num_joints = 13
+
+    model = STGCN(num_class=num_classes, num_point=num_joints)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
+    model = model.to(device)
+
+    action_loss = nn.CrossEntropyLoss()
+    phase_loss = nn.MSELoss()
+    optimizer = torch.optim.SGD(
+        model.parameters(),
+        lr=0.001,
+        momentum=0.9,
+        weight_decay=5e-4,
+        nesterov=True
+    )
+
+    num_epochs = 150
+    scheduler = CosineAnnealingLR(optimizer, T_max=num_epochs, eta_min=0)
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        correct_action = 0
+        total_action = 0
+        total_count_error = 0
+        total_count_samples = 0
+
+        for batch_x, batch_phase, batch_action in train_loader:
+            batch_x = batch_x.to(device)  # (N, C, T, V, M)
+            batch_action = batch_action.to(device)
+            batch_phase = batch_phase.to(device)
+            batch_phase = batch_phase[:, ::2]  # downsample phase labels to match model output
+
+            optimizer.zero_grad()
+            action_logits, phase = model(batch_x)
+            loss_action = action_loss(action_logits, batch_action)
+            loss_phase = phase_loss(phase, batch_phase.float())
+            loss = loss_action + loss_phase  # combine losses (weighthed)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item() * batch_x.size(0)
+
+            # action accuracy
+            _, predicted_action = torch.max(action_logits, 1)
+            correct_action += (predicted_action == batch_action).sum().item()
+            total_action += batch_action.size(0)
+
+            # phase prediction mae
+            total_count_error += torch.abs(phase.squeeze() - batch_phase.float()).sum().item()
+            total_count_samples += batch_phase.size(0)
+        
+        scheduler.step()
+
+        epoch_loss = running_loss / total_action
+        acc = correct_action / total_action if total_action > 0 else 0
+        mae = total_count_error / total_count_samples if total_count_samples > 0 else 0
+        
+        model.eval()
+        running_loss = 0.0
+        total_action = 0
+        for batch_x, batch_phase, batch_action in val_loader:
+            batch_x = batch_x.to(device)
+            batch_action = batch_action.to(device)
+            batch_phase = batch_phase.to(device)
+            batch_phase = batch_phase[:, ::2]  # downsample phase labels to match model output (every 4 frames)
+            
+            action_logits, phase = model(batch_x)
+            loss_action = action_loss(action_logits, batch_action)
+            loss_phase = phase_loss(phase, batch_phase.float())
+            loss = loss_action + loss_phase  # combine losses (weighthed)
+
+            running_loss += loss.item() * batch_x.size(0)
+            total_action += batch_action.size(0)
+            
+        val_loss = running_loss / total_action 
+        print(f"Epoch {epoch+1}/{num_epochs} | Loss: {epoch_loss:.4f} | Accuracy: {acc:.4f} | MAE: {mae:.4f}  | Val_Loss: {val_loss:.4f}")
+        
+        
+
+        if (epoch + 1) % 10 == 0:
+            torch.save(model.state_dict(), f"stgcn4_epoch{epoch+1}.pth")
+            print(f"Model checkpoint saved at epoch {epoch+1}")
+            
+        early_stopping(val_loss, model)
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+        
+    early_stopping.load_best_model(model)
+    # Evaluation on test set
+    model.eval()
+
+    total_count_error = 0
+    total_count_samples = 0
+
+    with torch.no_grad():
+        correct_action = 0
+        total_action = 0
+
+        for batch_x, batch_phase, batch_action in test_loader:
+            batch_x = batch_x.to(device)
+            batch_action = batch_action.long().to(device)
+            batch_phase = batch_phase.to(device)
+            batch_phase = batch_phase[:, ::2]  # downsample phase labels to match model output
+
+            action_logits, phase = model(batch_x)
+            _, predicted_action = torch.max(action_logits, 1)
+
+            # Action classification metrics
+            correct_action += (predicted_action == batch_action).sum().item()
+            total_action += batch_action.size(0)
+
+            # Phase prediction metrics (MAE example)
+            total_count_error += torch.abs(phase.squeeze() - batch_phase.float()).sum().item()
+            total_count_samples += batch_phase.size(0)
+
+        accuracy = correct_action / total_action
+        mae = total_count_error / total_count_samples
+        print(f"Test Accuracy: {accuracy:.4f}")
+        print(f"Test Average Rep Count Error: {mae:.4f}")
+    
+    # save model
+    torch.save(model.state_dict(), "stgcn_model4.pth")
+    print("Model saved.")
+
+
+
+               
